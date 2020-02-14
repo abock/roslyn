9f22560fa3f20055d0da977cf5d6ce92c6dc81bd
Aaron Bockover <abock@microsoft.com>
2020-02-14 12:19:50 -0500

    Add IDS_AbstractStaticInterfaceMembers

--- src/Compilers/CSharp/Portable/Errors/MessageID.cs
+++ src/Compilers/CSharp/Portable/Errors/MessageID.cs
@@ -187,6 +187,7 @@ namespace Microsoft.CodeAnalysis.CSharp
         IDS_FeatureSwitchExpression = MessageBase + 12763,
         IDS_FeatureAsyncUsing = MessageBase + 12764,
         IDS_FeatureLambdaDiscardParameters = MessageBase + 12765,
+        IDS_AbstractStaticInterfaceMembers = MessageBase + 12766,
     }
 
     // Message IDs may refer to strings that need to be localized.
@@ -295,6 +296,7 @@ namespace Microsoft.CodeAnalysis.CSharp
             {
                 // Preview features.
                 case MessageID.IDS_FeatureLambdaDiscardParameters: // semantic check
+                case MessageID.IDS_AbstractStaticInterfaceMembers: // semantic check
                     return LanguageVersion.Preview;
 
                 // C# 8.0 features.
